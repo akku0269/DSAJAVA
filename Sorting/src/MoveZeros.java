e81e838cadf3919f6b33cc57dd33edf81f9ff2c7
akku0269 <krishnakantsahu19@gmail.com>
2023-05-30 15:44:15 +0530

    comment

--- Sorting/src/MoveZeros.java
+++ Sorting/src/MoveZeros.java
@@ -1,18 +1,18 @@
 public class MoveZeros {
 
-    static void moveZeros(int [] arr) {
+    static void moveZeros(int[] arr) {
 
         int n = arr.length;
         for(int i=0 ; i<n-1 ; i++){
-            for(int j = 0; j< n-i-1 ; j++){
-                if(arr[j]==0 && arr[j+1]!=0){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
+        for (int j=0 ; j<n-i-1; j++) {
+            if (arr[j] == 0 && arr[j + 1] != 0) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
+}
     public static void main(String[] args) {
         int [] arr = {0,5,0,4,2,0,8,0};
         int n = arr.length;
