8d4ddcc9b8c4a3fe39819a035843eed39bf7ebc4
akku0269 <krishnakantsahu19@gmail.com>
2023-06-02 18:57:09 +0530

    comment

--- Stacks/src/InterviewQuestions/NextGreaterElementImp.java
+++ Stacks/src/InterviewQuestions/NextGreaterElementImp.java
@@ -21,9 +21,7 @@ public class NextGreaterElementImp {
         return res;
     }
 
-
     public static void main(String[] args) {
-
         int[] arr = {1, 3, 2, 1, 8, 6, 3, 4};
         int[] res = nextGreater1(arr);
         for (int i = 0; i < arr.length; i++) {
