7f5d131d40ea6513fd741c4dcfc9ba3f9a080c35
akku0269 <krishnakantsahu19@gmail.com>
2023-06-04 11:42:47 +0530

    comment

--- Search/src/BinarySearch/LastOccurence.java
+++ Search/src/BinarySearch/LastOccurence.java
@@ -4,19 +4,19 @@ public class LastOccurence {
     static int lastOcc(int[] arr , int x){
         int n = arr.length;
         int st = 0 , end = n-1;
-        int fo=-1;
+        int lo=-1;
 
         while(st<=end) {
             int mid = st + (end - st) / 2;
             if (arr[mid] == x) {
-                fo = mid;
+                lo = mid;
                 st = mid + 1;
             } else if (arr[mid]<x) {
                 st = mid+1;
             }else
                 end = mid-1;
         }
-        return fo;
+        return lo;
     }
     public static void main(String[] args) {
         int[] arr = { 1,2,5,4,8,6,5,2,5,2,5,4,5} ;
