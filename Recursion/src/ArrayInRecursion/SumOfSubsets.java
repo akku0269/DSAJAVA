87f6ab88032941c21e6dfc5b03ec2a489812f657
akku0269 <krishnakantsahu19@gmail.com>
2023-06-04 11:52:25 +0530

    comment

--- Recursion/src/ArrayInRecursion/SumOfSubsets.java
+++ Recursion/src/ArrayInRecursion/SumOfSubsets.java
@@ -2,22 +2,24 @@ package ArrayInRecursion;
 
 public class SumOfSubsets {
 
-    static void subsetSum(int[] arr ,int n ,  int idx , int currSum){
+    static void subsetSum(int[] arr  ,  int idx , int currSum){
+        int n = arr.length;
       if(idx>=n){
           System.out.println(currSum);
           return;
       }
 
         // curr idx +sum
-        subsetSum(arr,n,idx+1,currSum + arr[idx]);
+        subsetSum(arr,idx+1,currSum + arr[idx]);
 
         // curr ans
-        subsetSum(arr,n,idx+1,currSum);
+        subsetSum(arr,idx+1,currSum);
 
     }
     public static void main(String[] args) {
+
         int[] arr = {2,4,5};
-        subsetSum(arr, arr.length,0,0);
+        subsetSum(arr, 0,0);
 
     }
 }
